7bd0bf4c54ff155702314db885169aea56be6804
Elwin <elwin.carlos09@gmail.com>
2021-04-15 16:19:29 +0100

    change start button id to snake-case

--- src/components/StartButton/index.js
+++ src/components/StartButton/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const StartButton = () => {
     return (
-        <button id="startButton">
+        <button id="start-button">
             <h1>Welcome to my portfolio!</h1>
             <h4>Click on me to get started ...</h4>
         </button>
