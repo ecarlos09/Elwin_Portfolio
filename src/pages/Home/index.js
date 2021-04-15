899fadb4467b6bf84bc72c97e925a17885c5cf5f
Elwin <elwin.carlos09@gmail.com>
2021-04-15 16:51:01 +0100

    stub out handler for startButton click

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,8 @@ const Home = () => {
 
     const mainStart = useRef();
 
+    // const handleStart = () => notesEnd.current.scrollIntoView({ behavior: "smooth" })
+
     return (
         <h1 ref={mainStart}>Welcome to my site!</h1>
     );
