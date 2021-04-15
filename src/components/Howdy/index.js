88869f9ac38827bb57a0bee381dce5c191655052
Elwin <elwin.carlos09@gmail.com>
2021-04-15 17:07:31 +0100

    create component for portrait image

---
+++ src/components/Howdy/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.css';
+
+const Howdy = () => {
+    return (
+        <img src="https://i.imgur.com/WGS6L73.jpg" alt="Willy Wonka costume" />
+    )
+}
+
+export default Howdy;
