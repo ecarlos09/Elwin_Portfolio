eee82f7e1c8d9846e59c87b429f45a84063d7796
Elwin <elwin.carlos09@gmail.com>
2021-04-15 09:52:33 +0100

    amend App and Browser Routing import statements andadd instructions to target the root div container

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { App } from './App';
+import App from './App';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
     <React.StrictMode>
         <Router>
             <App />
         </Router>
-    </React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
