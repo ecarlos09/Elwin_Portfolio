4aca158b1e3f9a8c77bb49c9f470e97bdb85a475
Elwin <elwin.carlos09@gmail.com>
2021-04-15 18:20:22 +0100

    import and export github skills component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { Home } from './pages';
-import { Header, Game, Footer } from './layout';
+import { Header, Skills, Game, Footer } from './layout';
 
 const App = () => {
     return (
@@ -11,6 +11,7 @@ const App = () => {
             <Switch>
                 <Route exact path="/" component={Home} />
             </Switch>
+            <Skills />
             <Game />
             <Footer />
         </>
