4a0dead8acecb064f0209fe993837b5441580f01
Elwin <elwin.carlos09@gmail.com>
2021-04-15 11:01:45 +0100

    write useWindowSizecustom hook until all tests pass

--- src/hooks/useWindowSize/index.js
+++ src/hooks/useWindowSize/index.js
@@ -1,7 +1,34 @@
 import { useState, useEffect } from 'react';
 
-const useWindowSize = {
+const useWindowSize = () => {
+    const [ windowSize, setWindowSize ] = useState({
+        width: undefined,
+        height: undefined,
+        mobile: undefined
+    });
 
+    useEffect(() => {
+        // Create handler that calls on window resize
+        function handleResize() {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+                mobile: window.innerWidth <= 768
+            });
+        };
+
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+
+        // Immediately invoke handler to get current window size
+        handleResize();
+
+        // Remove the event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+
+    }, []);
+
+    return windowSize;
 }
 
 export default useWindowSize;
