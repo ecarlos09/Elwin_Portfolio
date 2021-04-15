73fbe2cb3b347ffa939e1a5623f0a5da74e18985
Elwin <elwin.carlos09@gmail.com>
2021-04-15 09:41:51 +0100

    stub out header and footer and export from layout directory

---
+++ src/layout/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Welcome!  Let's get started</h1>
+        </header>
+    );
+};
+
+export default Header;
