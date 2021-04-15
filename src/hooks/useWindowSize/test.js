be03229e23c52373d2fb893ba4452df7974d4ff0
Elwin <elwin.carlos09@gmail.com>
2021-04-15 10:36:17 +0100

    stub out test file for useWindowSize

---
+++ src/hooks/useWindowSize/test.js
@@ -0,0 +1,13 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import useWindowSize from '.';
+
+describe('useWindowSize', () => {
+    it('it returns the window size and height of a desktop', () => {
+
+    });
+
+    it('returns the window size and height of a mobile', () => {
+
+    });
+})
