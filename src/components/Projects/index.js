9042e0a1ee8c6c96f74e4a41de9082898cdc9925
Elwin <elwin.carlos09@gmail.com>
2021-04-16 13:58:41 +0100

    stub out Projects component

---
+++ src/components/Projects/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <section className="projects">
+            <div className="project-1">
+                <h4>consoul.log</h4>
+                <img />
+                <a></a>
+            </div>
+
+            <div className="project-2">
+                <h4>habite</h4>
+                <img />
+                <a></a>
+            </div>
+
+            <div className="project-3">
+                <h4>choosic-music</h4>
+                <img />
+                <a></a>
+            </div>            
+        </section>
+    )
+}
+
+export default Projects;
