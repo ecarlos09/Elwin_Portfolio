814865780510ea9e442880f7c5f02385e2a79061
Elwin <elwin.carlos09@gmail.com>
2021-04-16 18:14:39 +0100

    reposition content inside Technical skills

--- src/components/GithubStats/index.js
+++ src/components/GithubStats/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import mostUsed from '../../assets/most-used.jpg';
 import gitStats from '../../assets/git-stats.jpg';
@@ -8,8 +9,29 @@ import './style.css';
 const GithubStats = () => {
     return (
         <section className="git-stats" >
-            <img src={mostUsed} />
-            <img src={gitStats} />
+
+            <h2>Technical Skills</h2>
+
+            <div className="git-stats-content">                
+                <div className="git-stats">
+                    <img src={mostUsed} />
+                    <img src={gitStats} />
+                </div>
+                
+                <div id="git-stats-blurb">
+                    <p>The infographics displayed are an overview of my current skill set and version control habits this year.</p>
+                    <p>
+                        Other technical skills include:
+                        <ul>
+                            <li>...</li>
+                        </ul>
+                    </p>     
+                    <p>
+                        <NavLink to="/projects" activeClassName="current">Click here</NavLink> to view a list of my most recent projects!
+                    </p>           
+                </div>
+            </div>
+
         </section>
     )
 }
