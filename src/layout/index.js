69cc4ad6e82607d953b05e72f003ece2b9b35128
Elwin <elwin.carlos09@gmail.com>
2021-04-16 15:17:15 +0100

    import and export nav bar as required

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,4 +1,5 @@
 export { default as Header } from './Header';
+export { default as NavBar } from './NavBar';
 export { default as Skills } from './Skills';
 export { default as Game } from './Game';
 export { default as Footer } from './Footer';
