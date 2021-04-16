63d2deac31df7aa5ff4485182be8e59d2c5ec0e2
Elwin <elwin.carlos09@gmail.com>
2021-04-16 16:47:52 +0100

    resize and reposition project images

--- src/components/ProjectInfo/index.js
+++ src/components/ProjectInfo/index.js
@@ -5,41 +5,56 @@ import consoulLogFeed from '../../assets/consoul-log-feed.jpg';
 import habite from '../../assets/habite.jpg';
 import choosic from '../../assets/choosic.jpg';
 
+import './style.css';
+
 const ProjectInfo = () => {
     return (
         <section className="projects">
-            <div className="projects-list">
-                <div className="project-1">
+            <div id="project-1">
+                <div className="project-img">
                     <img src={consoulLogLanding} alt="Landing page for consoul.log"/>
                     <img src={consoulLogFeed} alt="Exampe feed for consoul.log"/>
+                </div>
+
+                <div className="project-links">
                     <a href="https://consoul-log.netlify.app/">
-                        <i class="fab fa-chrome"></i>
+                        <i className="fab fa-chrome"></i>
                     </a>
                     <a href="https://github.com/ecarlos09/consoul-log">
                         <i className="fab fa-github"></i>
                     </a>
+                </div>                
+            </div>
+
+            <div className="project-2">
+                <div className="project-img">
+                    <img src={habite}/>
                 </div>
 
-                <div className="project-2">
-                    <img src={habite}/>
+                <div className="project-links">
                     <a href="https://habite.netlify.app/">
-                        <i class="fab fa-chrome"></i>
+                        <i className="fab fa-chrome"></i>
                     </a>
                     <a href="https://github.com/ecarlos09/co-habiters">
                         <i className="fab fa-github"></i>
                     </a>
                 </div>
-
-                <div className="project-3">
+            </div>    
+            
+            <div className="project-3">
+                <div className="project-img">
                     <img src={choosic} />
+                </div>
+
+                <div className="project-links">
                     <a href="https://choosic-music.netlify.app/">
-                        <i class="fab fa-chrome"></i>
+                        <i className="fab fa-chrome"></i>
                     </a>
                     <a href="https://github.com/ecarlos09/ee-music-app">
                         <i className="fab fa-github"></i>
                     </a>
                 </div>
-            </div>            
+            </div>        
         </section>
     )
 }
