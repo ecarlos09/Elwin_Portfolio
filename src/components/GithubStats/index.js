6c1df907a7b6a7c200c09297a5161e0fc04ccc89
Elwin <elwin.carlos09@gmail.com>
2021-04-16 17:43:41 +0100

    wrap stats within a section

--- src/components/GithubStats/index.js
+++ src/components/GithubStats/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 import mostUsed from '../../assets/most-used.jpg';
 import gitStats from '../../assets/git-stats.jpg';
 
-import { Game } from '../../layout';
+import './style.css';
 
 const GithubStats = () => {
     return (
-        <>
+        <section className="git-stats" >
             <img src={mostUsed} />
             <img src={gitStats} />
-        </>
+        </section>
     )
 }
 
