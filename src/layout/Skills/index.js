55863b788b4d853701f63fe7518bcdb14f54bff8
Elwin <elwin.carlos09@gmail.com>
2021-04-16 14:59:47 +0100

    remove project info from skills section

--- src/layout/Skills/index.js
+++ src/layout/Skills/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import { GithubStats, Projects } from '../../components'
+import { GithubStats } from '../../components'
 
 const Skills = () => {
     return (
         <>
             <h2>Technical Skills</h2>
             <GithubStats />
-            <Projects />
         </>
     )
 }
