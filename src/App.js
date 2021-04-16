c0b36a789f49e7f697d0ed5a337bc6496583728b
Elwin <elwin.carlos09@gmail.com>
2021-04-16 15:16:21 +0100

    add nav bar to beginning of page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { Home, Projects } from './pages';
-import { Footer } from './layout';
+import { NavBar, Footer } from './layout';
 
 const App = () => {
     return (
         <>
+            <NavBar />
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/projects" component={Projects} />
