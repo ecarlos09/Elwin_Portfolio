ebf83592e7263d615ea2e2aeb94dc53c5a0a678c
Elwin <elwin.carlos09@gmail.com>
2021-04-17 06:55:22 +0100

    add styling to game area

--- src/components/PlayIcon/index.js
+++ src/components/PlayIcon/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import './style.css'
+
 const PlayIcon = () => {
     return (
-        <>
+        <section className="lets-play">
             <h3>Let's Play</h3>
             <img src="https://i.imgur.com/wBJ6lNy.png" alt="Play icon" />
             <p>Content loading ...check back later to see if I've finished it!</p>
-        </>
+        </section>
     )
 }
 
