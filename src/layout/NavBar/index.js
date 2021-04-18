882cc9ac8f4a8b7e2520aab307cb0b9d8b28f8cc
Elwin <elwin.carlos09@gmail.com>
2021-04-18 09:49:32 +0100

    add bootstrap styling to navbar

--- src/layout/NavBar/index.js
+++ src/layout/NavBar/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container }  from 'react-bootstrap';
+
 import './style.css';
 
 const NavBar = () => {
     return (
-        <nav>
-            <div className="links">
-                <NavLink exact to="/" activeClassName="current">Home</NavLink>
-                <NavLink to="/projects" activeClassName="current">Projects</NavLink>
-            </div>
-        </nav>
-
+        <Container fluid>
+            <Navbar expand="md" fixed="top">
+            <Navbar.Brand href="#home">Elwin's Portfolio</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <div className="links">
+                            <NavLink exact to="/" activeClassName="current">Home</NavLink>
+                            <NavLink to="/projects" activeClassName="current">Projects</NavLink>
+                        </div>
+                        <NavDropdown title="More" id="basic-nav-dropdown">
+                            <NavDropdown.Item>About</NavDropdown.Item>
+                            <NavDropdown.Item>Game</NavDropdown.Item>
+                            <NavDropdown.Item>Contact</NavDropdown.Item>
+                            {/* <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
+                        </NavDropdown>
+                    </Nav>
+                    {/* <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-success">Search</Button>
+                    </Form> */}
+                </Navbar.Collapse>
+            </Navbar>
+        </Container>
     )
 }
 
