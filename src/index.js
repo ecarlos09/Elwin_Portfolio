4fa84999b8e8ee4c68c271a0e3df8ac3906b009a
Elwin <elwin.carlos09@gmail.com>
2021-04-18 09:11:34 +0100

    import bootstrap for react

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
