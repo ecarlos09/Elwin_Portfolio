c5bee95682b5297b1f1e0cdf9c2af9bb0f712288
Elwin <elwin.carlos09@gmail.com>
2021-04-19 16:47:39 +0100

    pass handleStart prop to startButton

--- src/components/StartButton/index.js
+++ src/components/StartButton/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const StartButton = () => {
+const StartButton = ({handleStart}) => {
     return (
-        <button id="start-button">
+        <button id="start-button" onClick={handleStart}>
             <h1>Welcome to my portfolio!</h1>
             <h4>Click on me to get started ...</h4>
         </button>
