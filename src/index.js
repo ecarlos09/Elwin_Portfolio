2b570bae45d9b152d0fb08f2d7ce80b3d2e06823
Elwin <elwin.carlos09@gmail.com>
2021-04-19 09:38:56 +0100

    give app access to the store

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ import './index.css';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import { store } from './store';
+
 ReactDOM.render(
     <React.StrictMode>
         <Router>
-            <App />
+            <Provider store={store} >
+                <App />
+            </Provider>            
         </Router>
     </React.StrictMode>,
     document.getElementById('root')
