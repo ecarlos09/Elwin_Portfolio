19f7fef9e6ab8e03967df1ea235a0d46568f6c0d
Elwin <elwin.carlos09@gmail.com>
2021-04-19 09:25:36 +0100

    create reducer for play name

--- src/reducers/playerReducer.js
+++ src/reducers/playerReducer.js
@@ -1,4 +1,15 @@
 const initState = {
-    name: 'stranger',
-    addName: null
+    playerName: 'stranger'
 }
+
+const playerReducer =  (state=initState, action) => {
+    switch(action.type) {
+        case 'ADD_NAME':
+            const newPlayer = action.payload;
+            return { ...state, playerName: newPlayer};
+        default: 
+            return state;
+    }
+}
+
+export default playerReducer;
