68d42c410db2a876ae627041a66b2acd86bccc66
Elwin <elwin.carlos09@gmail.com>
2021-04-19 10:35:33 +0100

    add greeting and name submit form to Player Info component

--- src/components/PlayerInfo/index.js
+++ src/components/PlayerInfo/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useSelector } from 'react-redux';
 
 const PlayerInfo = () => {
+    const stranger = useSelector(state => state.players[0]);
+    console.log(stranger);
+    const [ player, setPlayer ] = useState(stranger);
+    const [ nameInput, setNameInput ] = useState("");
+
+    const handleInput = e => setNameInput(e.target.value);
+    
+    const handleNameSubmit = e => {
+        e.preventDefault();
+        setPlayer(nameInput);
+        setNameInput("");
+    }
+
     return (
-        <>
-            <p>Player Info will go here</p>
-        </>
+        <section className="player-name">
+            <h3>Hi there, { player }!</h3>
+            <form onSubmit={handleNameSubmit}>
+                <label htmlFor="username">Not your name?</label>
+                <input type="text" id="username" name="username" placeholder="Tell us" value={nameInput} onChange={handleInput} />
+                <input type="submit" value="We're waiting ..." />
+            </form>
+        </section>
     )
 }
 
