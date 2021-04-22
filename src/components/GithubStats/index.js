c4ff4c4932fd8c9f880e521b7b29fd63938d3879
Elwin <elwin.carlos09@gmail.com>
2021-04-22 18:32:43 +0100

    wrap git stats in fluid container

--- src/components/GithubStats/index.js
+++ src/components/GithubStats/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 import mostUsed from '../../assets/most-used.jpg';
 import gitStats from '../../assets/git-stats.jpg';
@@ -8,15 +9,11 @@ import './style.css';
 
 const GithubStats = () => {
     return (
-        <section className="git-stats" >
+        <Container className="git-stats" fluid>
 
             <h2>Technical Skills</h2>
 
-            <div className="git-stats-content">                
-                <div className="git-stats">
-                    <img src={mostUsed} />
-                    <img src={gitStats} />
-                </div>
+            <div className="git-stats-content">  
                 
                 <div id="git-stats-blurb">
                     <p>The infographics displayed are an overview of my current skill set and version control habits this year.</p>
@@ -35,9 +32,15 @@ const GithubStats = () => {
                         <NavLink to="/projects" activeClassName="current">Click here</NavLink> to view a list of my most recent projects!
                     </p>           
                 </div>
+
+                <div className="git-stats">
+                    <img src={mostUsed} />
+                    <img src={gitStats} />
+                </div>
+                
             </div>
 
-        </section>
+        </Container>
     )
 }
 
