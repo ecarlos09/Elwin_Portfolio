efee3d8058dd7a2f965a89c6b8921757b17d483f
Elwin <elwin.carlos09@gmail.com>
2021-04-22 18:35:14 +0100

    add thank you

---
+++ src/components/Thanks/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+import './style.css';
+
+const Thanks = () => {
+    return (
+        <Container className="thanks" fluid>
+            <h1>Thanks for visiting!</h1>
+        </Container>
+    )
+}
+
+export default Thanks;
