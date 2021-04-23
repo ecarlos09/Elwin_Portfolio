198c0134ade60050d203a3293e6d8b97805070c3
Elwin <elwin.carlos09@gmail.com>
2021-04-23 14:01:51 +0100

    wrap git stats in fluid container

--- src/layout/Skills/index.js
+++ src/layout/Skills/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import { Container } from 'react-bootstrap'
+
 import { GithubStats } from '../../components'
 
 const Skills = () => {
     return (
-        <>
+        <Container fluid>
             <GithubStats />
-        </>
+        </Container>
     )
 }
 
