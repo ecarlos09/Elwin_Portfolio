ccc4a2a316681256a99f25df3e3f1f3667b987c6
Elwin <elwin.carlos09@gmail.com>
2021-04-23 20:28:54 +0100

    change mobile notice from h1 to p

--- src/components/Howdy/index.js
+++ src/components/Howdy/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, Image } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { Intro } from '..';
 
 import './style.css';
@@ -9,10 +9,9 @@ const Howdy = () => {
     return (
         <Container className="howdy">
             <Container fluid className="title">
-                <h1>N.B.  We are in the process of making this more mobile friendly! </h1>
+                <p id="mob-note">N.B.  We are in the process of making this more mobile friendly! </p>
                 <h1>Howdy!</h1>
             </Container>
-            <Image id="howdy-img" src="https://i.imgur.com/WGS6L73.jpg" alt="Willy Wonka costume" fluid roundedCircle />
             <Intro />
         </Container>
     )
