75dca8e739838bf7095791a5d7dec40e410db0ec
Elwin <elwin.carlos09@gmail.com>
2021-04-23 14:02:41 +0100

    move intro blurb to howdy component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ import React, { useRef } from 'react';
 import { Container } from 'react-bootstrap';
 
 import { NavBar, Header, Skills, Game, Contact } from '../../layout';
-import { Howdy, Intro, Thanks } from '../../components';
+import { Howdy, Thanks } from '../../components';
 
 import './style.css';
 
@@ -39,12 +39,7 @@ const Home = () => {
             <NavBar handleStart={handleStart} handleSkills={handleSkills} handleGame={handleGame} handleContact={handleContact} />
 
             <section ref={mainStart} className="intro">
-                <h1>N.B.  This page is still under construction ... </h1>
-                <h1>Howdy!</h1>
-                <Container className="about" fluid="md">
-                    <Howdy id="howdy" />
-                    <Intro id="intro" />
-                </Container>            
+                <Howdy />         
             </section>
 
             <span ref={skills}></span>
