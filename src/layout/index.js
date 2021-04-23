4d73f43587703ce870676f8a94dd9907076e4c48
Elwin <elwin.carlos09@gmail.com>
2021-04-23 10:43:32 +0100

    export project page navbar

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,5 +1,6 @@
 export { default as Header } from './Header';
 export { default as NavBar } from './NavBar';
+export { default as ProjectNavBar } from './ProjectNavBar';
 export { default as Skills } from './Skills';
 export { default as Game } from './Game';
 export { default as Contact } from './Contact';
