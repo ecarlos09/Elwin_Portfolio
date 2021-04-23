42b2727c44a1f0be7869989c8a6b0b771b6d89e5
Elwin <elwin.carlos09@gmail.com>
2021-04-23 14:00:31 +0100

    move title to howdty component

--- src/components/Howdy/index.js
+++ src/components/Howdy/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import { Container, Image } from 'react-bootstrap';
+import { Intro } from '..';
 
 import './style.css';
 
 const Howdy = () => {
     return (
-        <Container fluid>
+        <Container className="howdy">
+            <Container fluid className="title">
+                <h1>N.B.  We are in the process of making this more mobile friendly! </h1>
+                <h1>Howdy!</h1>
+            </Container>
             <Image id="howdy-img" src="https://i.imgur.com/WGS6L73.jpg" alt="Willy Wonka costume" fluid roundedCircle />
+            <Intro />
         </Container>
     )
 }
